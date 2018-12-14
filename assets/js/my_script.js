607335fe462fd719566b4318041d1be3b48842bf
sylvrebos5123 <sylvievrebos@gmail.com>
2018-12-14 15:20:19 +0100

    back-end PHP/SQL + some changes in JS

--- assets/js/my_script.js
+++ assets/js/my_script.js
@@ -5,6 +5,27 @@ function goTo(page,choice){
     xhttp.send();
     document.getElementById("content").innerHTML = xhttp.responseText;
 
+    if(page=="step3_no")
+    {
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                myObj = JSON.parse(this.responseText);
+                //to display the object in the console
+                console.log(myObj);
+                var list='';
+                for(var x=0;x<myObj.length;x++){
+                    list += "<div class=\"btn_ingredient\" onclick=\"goTo('step3_choice','"+myObj[x].label+"')\">";
+                    list += myObj[x].label;
+                    list += "</div>";
+                }
+                document.getElementById("list_ingredients").innerHTML = list;
+                
+            }
+        };
+        xhttp.open("GET", "./php/list_ingredients.php", true);
+        xhttp.send();
+    }
+
     if(page == 'step3_choice'){
         document.getElementById("choice").innerHTML=choice;
     }
